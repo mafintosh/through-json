974d79643c956085a1465a892184a8bfb2307157
Mathias Buus <mathiasbuus@gmail.com>
2014-03-21 00:21:17 +0100

    added tests

---
+++ test.js
@@ -0,0 +1,22 @@
+var test = require('tap').test;
+var parse = require('through-json');
+
+test('parse 1 message', function(t) {
+	t.plan(1);
+	parse(function(data) {
+		t.same(data, {hello:'world'});
+	}).write('{"hello":"world"}');
+});
+
+test('parse 2 messages', function(t) {
+	t.plan(2);
+
+	var expected = [{hello:'world'},{hello:'verden'}];
+
+	var p = parse(function(data) {
+		t.same(data, expected.shift());
+	})
+
+	p.write('{"hello":"world"}');
+	p.end('{"hello":"verden"}');
+});
